eef97bfb7c243a60f59876d8120e8a64b7d22897
xder414 <xder414@gmail.com>
2024-11-06 17:18:55 +0700

    Memperbaiki format output hasil perhitungan

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 #meminta input panjang dan panjang dari penguna
 panjang = float(input("masukan panjang persegi panjang : "))
-lebar = float (input("masukan lebar persgi panjang: "))
+lebar = float(input("masukan lebar persgi panjang: "))
 
 #menghitung luas 
-luas = panajng * lebar
+luas = panjang * lebar
 
 #mengitung keliling 
-keliling = 2 * (panjang + leabar)
+keliling = 2 * (panjang + lebar)
 
 #menampulkan hasil luas dan keliling 
-print (f"luas persegi panjang adalah: {luas}")
-print (f"keliling persegi panjang dalah: {keliling}")
+print (f"luas persegi panjang adalah: {luas} satuan persegi")
+print (f"keliling persegi panjang dalah: {keliling} satuan")
