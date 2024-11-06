25b5fcdc43ae2ab6f5f932bbe1899126a82d7a92
rifqi141209 <pone55241@gmail.com>
2024-11-06 18:13:01 +0700

    merge branch validasi-input ke main

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
+#fungsi untuk memeriksa apakah input adalah bilangan positif
+def input_positif(prompt):
+    while True:
+        try:
+            nilai = float(input(prompt))
+            if nilai > 0:
+                return nilai 
+            else:
+                prin("nilai harus lebih dari 0. coba lagi")
+        except valuError:
+            print("input tidak valid. masukan angka") 
+               
 #meminta input panjang dan panjang dari penguna
 panjang = float(input("masukan panjang persegi panjang : "))
 lebar = float(input("masukan lebar persgi panjang: "))
@@ -9,5 +21,5 @@ luas = panjang * lebar
 keliling = 2 * (panjang + lebar)
 
 #menampulkan hasil luas dan keliling 
-print (f"luas persegi panjang adalah: {luas}")
-print (f"keliling persegi panjang dalah: {keliling}")
+print (f"luas persegi panjang adalah: {luas} satuan persegi")
+print (f"keliling persegi panjang dalah: {keliling} satuan")
